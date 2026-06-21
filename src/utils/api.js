7dbfb697c907de08f45e1d9497b602039ac5728d
TradingMorning <falconspido.com@gmail.com>
2026-06-21 09:17:31 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Detect host environment
 const API = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'http://localhost:5000/api/v1',
   headers: {
     'Content-Type': 'application/json'
   }
