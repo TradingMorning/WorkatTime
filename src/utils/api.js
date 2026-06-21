d6b45ab0b9020ce8d54a02c7e91b406ab8533a61
TradingMorning <falconspido.com@gmail.com>
2026-06-21 08:56:26 +0530

    Change API base URL to localhost for development

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Detect host environment
 const API = axios.create({
-  baseURL: '/api/v1',
+  baseURL: 'http://localhost:5000',
   headers: {
     'Content-Type': 'application/json'
   }
